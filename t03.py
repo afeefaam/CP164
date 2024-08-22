71795370aa9a65d4b2e490ea1d070cebf1eff08a
Afeefa Malik <afeefam.am@gmail.com>
2024-08-22 17:11:54 -0400

    Add files via upload

---
+++ t03.py
@@ -0,0 +1,26 @@
+"""
+------------------------------------------------------------------------
+[This program tests task 3]
+------------------------------------------------------------------------
+Author: Afeefa Malik
+ID:     169060299
+Email:  mali0299@mylaurier.ca
+__updated__ ="2024-02-02"
+------------------------------------------------------------------------
+"""
+from List_array import List
+
+key=List()
+key.insert(0,0)
+key.insert(1,1)
+key.insert(2,2)
+key.insert(3,3)
+key.insert(4,4)
+key.insert(5,5)
+index=key.index(2)
+key.__contains__(7)
+
+for value in key:
+    print(value)
+    
+    
